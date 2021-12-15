1187574de279af90460a7e38f6dcea3a9378a43e
Vik <v8hdev@gmail.com>
2021-12-15 18:12:28 +0200

    Add my project

---
+++ src/components/Header/Header.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import {Container, Form, FormControl, Nav, Navbar, Button} from 'react-bootstrap';
+import {Routes, Route,} from 'react-router-dom';
+import './Header.css';
+
+import Catalog from '../Catalog/Catalog';
+import News from '../News/News';
+
+function Header() {
+  return (
+    <div className="Header">
+        <Navbar collapseOnSelect exppand="md" bg="dark" variant="dark">
+            <Container>
+                <Navbar.Toggle aria-controls='responsive-navbar-nav'/>
+                <Navbar.Collapse className='nav-col' id='responsive-navbar-nav'>
+                    <Nav className='mr-auto'>
+                        <Nav.Link href='/'>Home</Nav.Link>
+                        <Nav.Link href='/news'>News</Nav.Link>
+                        <Nav.Link href='/catalog'>Catalog</Nav.Link>
+                    </Nav>
+                    <Form inline className='nav-form'>
+                        <FormControl 
+                            type='text'
+                            placeholder='Search'
+                            className='mr-sm-2'
+                        />
+                        <Button variant='outline-info'>Search</Button>
+                    </Form>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+
+        <Routes>
+            <Route path='/news' element={<News />} />
+            <Route path='/catalog' element={<Catalog />}/>
+        </Routes>
+    </div>
+  );
+}
+
+export default Header;
---
+++ src/components/News/News.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './News.css';
+
+function News() {
+  return (
+    <div className="News">
+      <h1>News</h1>
+    </div>
+  );
+}
+
+export default News;
